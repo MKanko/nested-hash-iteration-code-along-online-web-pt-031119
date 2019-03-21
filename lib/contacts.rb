abc467330fd0919c52afb7a828154c5278e6f6be
Mark Kanko <mkanko66@gmail.com>
2019-03-21 04:23:57 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, personal_data_hash|
-    personal_data_hash.eacg do |attribute, att_details|
+    personal_data_hash.each do |attribute, att_details|
       if attribute == :favorite_ice_cream_flavors
 binding.pry
       end
